90d318a226239e47cd2eef14c48e088cc2336056
Liam Gallagher <liam.d.gallagher@protonmail.com>
2022-01-28 17:57:01 +0000

    Update vulkano so Descriptor sets can be used on shaders

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ mod realtime_fft;
 use std::time::{Duration, Instant};
 
 fn main() {
-    // application::Application::new();
-    let stream = audio_input::InputStream::new();
-    let mut dft = realtime_fft::RealtimeFft::new(stream, Duration::from_secs_f64(0.02));
-    let sleep_time = Duration::new(1, 0) / dft.sample_rate();
-    loop {
-        //        let now = Instant::now();
-        dft.update();
-        println!("{}", dft.dft().borrow()[3]);
-        std::thread::sleep(sleep_time)
-    }
+    application::Application::new();
+    //let stream = audio_input::InputStream::new();
+    //let mut dft = realtime_fft::RealtimeFft::new(stream, Duration::from_secs_f64(0.02));
+    //let sleep_time = Duration::new(1, 0) / dft.sample_rate();
+    //loop {
+    //    //        let now = Instant::now();
+    //    dft.update();
+    //    println!("{}", dft.dft().borrow()[3]);
+    //    std::thread::sleep(sleep_time)
+    //}
 }
